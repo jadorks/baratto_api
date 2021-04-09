a725fdd32f25e17284d822f11bc9a7854e5ce124
fiifibentil <51439542+fiifibentil@users.noreply.github.com>
2021-04-09 23:56:52 +0000

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Todo(models.Model):
-    title = models.CharField(max_length = 100)
-    body = models.CharField(max_length = 100)
-    is_completed = models.BooleanField(default=False)
-    date_created = models.DateField(auto_created=True)
-    last_modified = models.DateField(auto_now=True)
+class Users(models.Model):
+    Fname = models.CharField(max_length=50)
+    Lname = models.CharField(max_length=50)
+    Email = models.CharField(max_length=50)
+    DOB = models.DateField()
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER)
+    Phonenumber = models.CharField(max_length=15)
+    Address = models.CharField(max_length=50)
+    OtherSocialMedia = models.CharField(max_length=50)
+    
+
+class Products(models.Model):
+    UserID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    Name = models.CharField(max_length = 100)
+    Price = models.IntegerField()
+    Description = models.CharField(max_length = 100)
+    
 
     def __str___(self):
         return self.title 
